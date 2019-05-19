4738664d0da8fc59546e9a3fb539f0522c2c52b2
Paul Greenham <paul.greenham@gmail.com>
2019-05-19 16:00:52 +0300

    busy converting to mobx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import axios from 'axios'
+import { observer } from 'mobx-react'
 
 import Home from './components/Home'
 import AddBudgetItem from './components/AddBudgetItem'
@@ -9,6 +10,7 @@ import CreateBudget from './components/CreateBudget'
 
 import './App.css'
 
+@observer
 class App extends Component {
   constructor() {
     super()
