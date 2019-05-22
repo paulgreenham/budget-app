c5db05f4f625e288370d5ef0e9bc6cae29fa9f00
Paul Greenham <paul.greenham@gmail.com>
2019-05-22 22:38:56 +0300

    modifications for heroku deployment

--- server.js
+++ server.js
@@ -5,25 +5,29 @@ const api = require('./server/routes/api')
 
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/budgetDB', {useNewUrlParser: true})
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budgetDB', {useNewUrlParser: true})
 mongoose.set('useFindAndModify', false);
 
 
 const app = express()
+app.use(express.static(path.join(__dirname, 'build')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
-
-    next()
+    if (process.env.PORT) { return }
+    else {
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+        next()
+    }
 })
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use('/', api)
 
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
 
 const port = 3723
-app.listen(port, function () {
-    console.log(`Server running on port ${port}`)
-})
+app.listen(process.env.PORT || port)
