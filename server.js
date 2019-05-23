2db67defb466669b497e0a5454991e4a228898f5
Paul Greenham <paul.greenham@gmail.com>
2019-05-23 10:03:44 +0300

    testing routes

--- server.js
+++ server.js
@@ -5,27 +5,28 @@ const api = require('./server/routes/api')
 
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_PATH || 'mongodb://localhost/budgetDB', {useNewUrlParser: true})
-mongoose.set('useFindAndModify', false);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budgetDB', {useNewUrlParser: true})
+mongoose.set('useFindAndModify', false)
 
 
 const app = express()
 app.use(express.static(path.join(__dirname, 'build')))
-app.use(express.static(path.join(__dirname, 'node_modules')))
-app.use(function (req, res, next) {
-    if (process.env.PROD) { return }
-    else {
-        res.header('Access-Control-Allow-Origin', '*')
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
-        next()
-    }
-})
+// app.use(express.static(path.join(__dirname, 'node_modules')))
+// app.use(function (req, res, next) {
+//     if (process.env.PROD) { return }
+//     else {
+//         res.header('Access-Control-Allow-Origin', '*')
+//         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+//         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+//         next()
+//     }
+// })
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use('/', api)
 
 app.get('*', function (req, res) {
+    console.log("what is happening")
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
