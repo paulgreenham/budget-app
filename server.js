54afbc1d1c093e80ac3ae6421a0fda30496f8fcd
Paul Greenham <paul.greenham@gmail.com>
2019-06-20 11:56:24 +0300

    minor changes to allow dev and production running without having to change everything when switching

--- server.js
+++ server.js
@@ -10,21 +10,23 @@ mongoose.set('useFindAndModify', false)
 
 
 const app = express()
-app.use(express.static(path.join(__dirname, 'build')))
-// app.use(function (req, res, next) {
-//     if (!process.env.NODE_ENV === "production") {
-//         res.header('Access-Control-Allow-Origin', '*')
-//         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-//         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
-//         next()
-//     }
-// })
+if (!process.env.NODE_ENV === "production") {
+    console.log("here")
+    app.use(function (req, res, next) {
+            res.header('Access-Control-Allow-Origin', '*')
+            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+            res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+            next()
+        })
+}
+else {
+    app.use(express.static(path.join(__dirname, 'build')))
+}
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use('/', api)
 
 app.get('*', function (req, res) {
-    console.log("what is happening")
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
